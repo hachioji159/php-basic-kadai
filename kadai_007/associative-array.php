fa367b9cb21c83380c77b9ffaf81b513b7a1338b
hachioji159 <ithachioji159@gmail.com>
2024-05-24 11:13:45 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題7</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $food = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($food);
+
+    ?>
+  </p>
+</body>
+
+</html>
